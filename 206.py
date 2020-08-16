076de9a0b68723ccb862205abea17de67582f11b
Sushant <35959118+ahujasushant@users.noreply.github.com>
2020-08-16 00:12:13 -0400

    Update the solution for the reverse linked list

--- 206.py
+++ 206.py
@@ -6,12 +6,8 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         curr_node = head
-        if curr_node is None:
-            new_curr_node = None
-            return new_curr_node
-        else:
-            new_curr_node = ListNode(curr_node.val, None)
-        while curr_node.next:
-            new_curr_node = ListNode(curr_node.next.val, new_curr_node)
+        new_curr_node = None
+        while curr_node:
+            new_curr_node = ListNode(curr_node.val, new_curr_node)
             curr_node = curr_node.next
         return new_curr_node
