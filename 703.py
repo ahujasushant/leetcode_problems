85e7cfcbd3c8e741c6998cf5018f0d9ff0301251
sushantahujaa <sushantahuja35@gmail.com>
2021-01-17 16:35:09 -0500

    703. Kth Largest Element in a Stream (another)

--- 703.py
+++ 703.py
@@ -11,3 +11,22 @@ class KthLargest:
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+#####
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        heapq.heapify(nums)
+        self.k = k
+
+        while len(nums) > k:
+            heapq.heappop(nums)
+        self.nums = nums
+
+    def add(self, val: int) -> int:
+        if len(self.nums) < self.k:
+            heapq.heappush(self.nums, val)
+        elif val > self.nums[0]:
+            heapq.heapreplace(self.nums, val)
+
+        return self.nums[0]
